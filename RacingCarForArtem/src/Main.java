22720e6666a7eade6edeca37f02d80bcfbd4db20
Ololosh2332 <122698243+Ololosh2332@users.noreply.github.com>
2023-01-29 13:20:32 +0300

    v. чуть чуть посовершенней
    Убрал лишние гетеры, добавил отображеие победителя. Формулы рассчета времени круга тупые, но я не гений геймдизайнер, иначе делал бы игру про ходьбу по неровным поверхностям.

--- RacingCarForArtem/src/Main.java
+++ RacingCarForArtem/src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,8 +17,10 @@ public class Main {
             int age = sc.nextInt();
             Racer racer = new Racer (racerId, name, age);
             racerList.add(racer);
+            System.out.println("Выберете машину Гонщика: \n1 - BMW\n2 - Subaru\n3 - Ford\n4 - Ferrari");
+            racerList.get(i).setCar(sc.nextInt());
             System.out.println("Добавлена информация о гонщике: ");
-            System.out.println("id гонщика "+racerList.get(i).speakId(racerId)+"\nИмя гонщика "+racerList.get(i).speakName(name)+"\nВозраст гонщика "+racerList.get(i).speakAge(age)+"\n\n");
+            System.out.println("id гонщика "+racerList.get(i).speakId(racerId)+"\nИмя гонщика "+racerList.get(i).speakName(name)+"\nВозраст гонщика "+racerList.get(i).speakAge(age)+"\n"+racerList.get(i).getCar(racerList.get(i).name, racerList.get(i).car)+"\n");
         }
 
         for (int i = 0; i<countRacer; i++){
@@ -27,29 +28,34 @@ public class Main {
             racerList.get(i).personalProblems(racerList.get(i).age);
             racerList.get(i).luckCalc(racerList.get(i).age, racerList.get(i).preparation, racerList.get(i).personalProblems);
         }
-        for (int i = 0; i<racerList.size(); i++){
+       /* for (int i = 0; i<racerList.size(); i++){
             System.out.println(racerList.get(i).speakId(racerList.get(i).racerId));
             System.out.println(racerList.get(i).speakName(racerList.get(i).name));
             System.out.println(racerList.get(i).speakAge(racerList.get(i).age));
-        }
+        } */
         System.out.println("Введите количество кругов: ");
         int r = sc.nextInt();
         for (int i = 0; i<countRacer; i++){
             for (int j = 1; j<=r; j++){
-                double rt = ringTime(racerList.get(i).luck, racerList.get(i).preparation, racerList.get(i).personalProblems);
+                double rt = Racer.ringTime(racerList.get(i).luck, racerList.get(i).preparation, racerList.get(i).personalProblems, racerList.get(i).car);
                 System.out.println("Круг "+j+" для гонщика "+racerList.get(i).speakName(racerList.get(i).name));
-                System.out.println("\nРезультат круга равен "+rt);
+                System.out.println(". Результат круга равен "+rt);
                 racerList.get(i).setTotalTime(rt);
             }
         }
-        System.out.println("\n\n");
+        System.out.println("\n");
         for (int i=0; i<countRacer; i++){
             racerList.get(i).getTotalTime(racerList.get(i).totalTime, racerList.get(i).name);
         }
+        double forWinnerTime = racerList.get(0).totalTime;
+        String winnerName = racerList.get(0).name;
+        for (int i = 1; i<countRacer; i++){
+            if (racerList.get(i).totalTime<forWinnerTime){
+                forWinnerTime=racerList.get(i).totalTime;
+                winnerName = racerList.get(i).name;
+            }
+        }
+        System.out.println("\n*****\nПобедителем становится "+winnerName+"\nЕго время: "+forWinnerTime);
     }
 
-    public static double ringTime (double luck, double preparation, double personalProblem){
-        double ringTime = luck*preparation*personalProblem+60.00*Math.random();
-        return ringTime;
-    }
 }
--- RacingCarForArtem/src/Racer.java
+++ RacingCarForArtem/src/Racer.java
@@ -6,6 +6,7 @@ public class Racer {
     double preparation;
     double personalProblems;
     boolean live = true;
+    String car;
 
     double totalTime;
 
@@ -14,7 +15,7 @@ public class Racer {
         this.name = name;
         this.age = age;
     }
-    public String speakName (String name){
+     public String speakName (String name){
         return name;
     }
     public int speakAge (int age){
@@ -37,5 +38,40 @@ public class Racer {
     }
     public static void getTotalTime (double totalTime, String name){
         System.out.println("общее время гонщика "+name+": "+totalTime);
+    }
+
+    public static String getCar (String name, String car){
+        return "Машина гонщика "+name+" это "+car;
+    }
+    public void setCar (int car){
+        if (car==1){
+            this.car = "BMW";
+        }
+        if (car==2){
+            this.car = "Subaru";
+        }
+        if (car==3){
+            this.car="Ford";
+        }
+        if (car==4){
+            this.car="Ferrari";
+        }
+    }
+    public static double ringTime (double luck, double preparation, double personalProblem, String car){
+        double ringTime = luck*preparation*personalProblem+60.00*Math.random();
+        if (car.equals("BMW")){
+            ringTime = ringTime*0.97;
+        }
+        if (car.equals("Subaru")){
+            ringTime = ringTime*0.92;
+        }
+        if (car.equals("Ford")){
+            ringTime = ringTime*1.1;
+        }
+        if (car.equals("Ferrari")){
+            ringTime = ringTime*0.85;
+        }
+
+        return ringTime;
     }
 }
